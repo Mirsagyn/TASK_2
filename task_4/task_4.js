af5158a667c8da4c7a4d54e98057eae102319c14
Mirlan Sagynbaev <mirlan.sagynbaev.alt@gmail.com>
2022-05-26 10:39:58 +0600

    Updated

--- task_4/task_4.js
+++ task_4/task_4.js
@@ -1,8 +1,8 @@
 // Даны два числа a, b. 
 // Написать программу, которая считает среднее арифметическое двух чисел.
 
-let a = parseInt(prompt("1-чи сан?", ''));
-let b = parseInt(prompt("2-чи сан?", ''));
+let a = parseInt(prompt("1-чи сан?",));
+let b = parseInt(prompt("2-чи сан?",));
 
 let result = (a + b) / 2;
 
